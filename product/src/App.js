c692045ae51689a92c9297bfd67b76e99f591b56
Believe <117173080+Beve-hub@users.noreply.github.com>
2023-09-19 09:52:00 -0600

    second

--- product/src/App.js
+++ product/src/App.js
@@ -1,13 +1,13 @@
 import Dashboard from "./Features/Dashboard";
 import LandingPage from "./Features/LandingPage";
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<LandingPage/>}/>
-        <Route path='Profile' element={<Dashboard/>}/>
+        <Route index element={<LandingPage />} />
+        <Route path="Profile" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
